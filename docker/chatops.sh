c3f10932245d62687b36b3b28dba8639a9cfd43e
Al Maline <amaline@users.noreply.github.com>
2015-07-27 15:33:41 -0400

    updated script [ci skip]

--- docker/chatops.sh
+++ docker/chatops.sh
@@ -1,2 +1,2 @@
 mkdir -p /opt/stackstorm/packs/chatops/{actions,rules,sensors,aliases}
-cp aliases/* /opt/stackstorm/packs/chatops/aliases
+cp /root/st2/aliases/* /opt/stackstorm/packs/chatops/aliases
